d75a127d1f8f98d6175ff409127c12fab0eb5fe1
Student <example@example.com>
2020-01-30 15:02:15 -0800

    add authors to homepage with api call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    response = HTTParty.get('http://localhost:3000/authors', format: :plain)
+    @parsed = JSON.parse response, symbolize_names: true
     render :index
   end
 
